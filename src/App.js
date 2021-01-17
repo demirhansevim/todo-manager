c676f3dcae26a2ec15f98d8f03b371852a4b2dbc
demirhansevim <72740003+demirhansevim@users.noreply.github.com>
2021-01-17 22:02:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./bootstrap.css";
 import "./App.css";
 import "./todoStyle.css";
